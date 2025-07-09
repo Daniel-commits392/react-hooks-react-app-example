2c00f90e26886b5813bfde57ded56463fe3990f8
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-07-09 23:27:14 +0300

    changes made

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,21 +5,23 @@ import App from "../components/App";
 
 beforeEach(() => {
   render(<App />);
+  
 });
 
 test('should include "Now" in the header instead of a time', () => {
   expect(
     screen.queryByText(format(new Date(), "MMMM do yyyy, h:mm:ss a"))
   ).not.toBeInTheDocument();
-  expect(screen.queryByText(/Now/g)).toBeInTheDocument();
+  expect(screen.getByText(/Now/g)).toBeInTheDocument();
 });
 
 test("should include the <ExampleComponent />", () => {
-  expect(screen.queryByText("Whoa!")).toBeInTheDocument();
+  expect(screen.getByText("Whoa!")).toBeInTheDocument();
 });
 
 test("should include the <TestComponent />", () => {
-  expect(screen.queryByTitle("time video")).toBeInTheDocument();
+  expect(screen.getByTitle("time video")).toBeInTheDocument();
+  
 });
 
 //   it('should include "Now" in the header instead of a time', () => {
